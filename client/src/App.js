d42fe91301b19146b910dc544c6bdea7b673b33d
DanBodell <dannbodell@gmail.com>
2020-11-30 12:16:57 -0700

    Add title component to site

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './SiteComponents/title.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/SiteComponents/title.js
@@ -0,0 +1,13 @@
+import './title.css'
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Daniel Bodell</h1>
+      <hr></hr>
+      <h2>Consulting. Development. Design.</h2>
+    </div>
+  );
+}
+
+export default Title;
